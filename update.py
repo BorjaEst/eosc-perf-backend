feaf8208edf40b9123e673ef4779358166e80975
Borja Esteban <boressan@outlook.com>
2022-06-07 12:15:51 +0200

    Add time steps

--- update.py
+++ update.py
@@ -1,9 +1,12 @@
-import requests
 import logging
+import time
+
+import requests
 
 logging.basicConfig(level="INFO")
 logger = logging.getLogger("Update")
 api = "http://is.marie.hellasgrid.gr/rest"
+time_steps = []
 
 
 # GET sites ---------------------------------------------------------
@@ -30,21 +33,30 @@ def get_endpoints(site, params={"limit": -1}):
         raise Exception(f"{req.status_code}")
 
 
-# Composed sites ----------------------------------------------------
+# Help functions ----------------------------------------------------
 def site_name(site, endpoint):
     return f"{site['name']}: {endpoint['endpointImplementationName']}"
 
 
+def time_step():
+    time_steps.append(time.time())
+
+
 # Main call ---------------------------------------------------------
 if __name__ == "__main__":
+    time_step()  # Collect all sites
     sites = get_sites()
+
+    time_step()  # Collect site endpoints
     for site in sites:
         site['endpoints'] = get_endpoints(site)
 
+    time_step()  # Generate site names
     names = []
     for site in sites:
         names += [site_name(site, ep) for ep in site['endpoints']]
 
+    time_step()  # Print site names
     print(names)
 
     pass
